635ffeb5c1201c6c73a4c57e8690c329d84739d0
whitedustmoon1175 <whitedustmoon@gmail.com>
2025-03-09 23:40:56 +0800

    upgrade CN edition

--- OverlayPlugin.Core/EventSources/FFXIVClientStructsEventSource.cs
+++ OverlayPlugin.Core/EventSources/FFXIVClientStructsEventSource.cs
@@ -1,10 +1,9 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Windows.Forms;
 using Newtonsoft.Json.Linq;
+using RainbowMage.OverlayPlugin.MemoryProcessors;
 using RainbowMage.OverlayPlugin.MemoryProcessors.AtkStage;
-using FFXIVClientStructs.FFXIV.Client.UI;
-using FFXIVClientStructs.FFXIV.Component.GUI;
-using Newtonsoft.Json.Serialization;
 
 namespace RainbowMage.OverlayPlugin.EventSources
 {
@@ -15,14 +14,13 @@ namespace RainbowMage.OverlayPlugin.EventSources
         public int TrustCount;
         public byte PetCount;
         public byte ChocoboCount;
-        public List<Entry> Entries = new();
+        public List<Entry> Entries = new List<Entry>();
 
         public struct Entry
         {
             public int Index;
             public string Name;
             public EntryType Type;
-
             public enum EntryType
             {
                 Party = 0,
@@ -33,74 +31,83 @@ namespace RainbowMage.OverlayPlugin.EventSources
 
     public class FFXIVClientStructsEventSource : EventSourceBase
     {
-        private readonly IAtkStageMemory atkStageMemory;
+        private IAtkStageMemory atkStageMemory;
+        private FFXIVMemory memory;
 
         public BuiltinEventConfig Config { get; set; }
 
         public FFXIVClientStructsEventSource(TinyIoCContainer container) : base(container)
         {
+            var haveAtkStageMemory = container.TryResolve(out atkStageMemory);
+            if (!haveAtkStageMemory)
+            {
+                Log(LogLevel.Warning, "Could not construct FFXIVClientStructsEventSource: Missing atkStageMemory");
+                return;
+            }
             atkStageMemory = container.Resolve<IAtkStageMemory>();
-            
+            memory = container.Resolve<FFXIVMemory>();
+
             RegisterEventHandler("getFFXIVCSAddonSlow", (msg) =>
             {
                 var key = msg["name"]?.ToString();
-                return key == null ? null : GetAddon(key);
+                if (key == null)
+                    return null;
+                return GetAddon(key);
             });
 
-            RegisterEventHandler("getSortedPartyList", (_) => GetSortedPartyList());
+            RegisterEventHandler("getSortedPartyList", (msg) =>
+            {
+                return GetSortedPartyList();
+            });
         }
 
-        private unsafe JObject GetSortedPartyList()
+        private JObject GetSortedPartyList()
         {
             var partyList = new SortedPartyList();
 
-            if (!atkStageMemory.IsValid()) return null;
+            if (!atkStageMemory.IsValid())
+            {
+                return null;
+            }
 
-            var addonPartyListCandidate = atkStageMemory.GetAddon<AddonPartyList>();
-            if (!addonPartyListCandidate.HasValue) return null;
-            var addonPartyList = addonPartyListCandidate.Value;
+            dynamic addonPartyList = atkStageMemory.GetAddon("_PartyList");
 
+            if (addonPartyList == null)
+            {
+                logger.Log(LogLevel.Error, "addonPartyList not found");
+                return null;
+            }
 
             partyList.ChocoboCount = addonPartyList.ChocoboCount;
             partyList.MemberCount = addonPartyList.MemberCount;
-            partyList.PartyType = addonPartyList.PartyTypeTextNode->NodeText.ToString();
+            partyList.PartyType = addonPartyList.PartyTypeTextNode.NodeText;
             partyList.PetCount = addonPartyList.PetCount;
             partyList.TrustCount = addonPartyList.TrustCount;
 
-            for (var i = 0; i < partyList.MemberCount; ++i)
+            for (int i = 0; i < partyList.MemberCount; ++i)
             {
-                partyList.Entries.Add(PartyMemberToEntry(addonPartyList.PartyMembers, i,
-                                                         SortedPartyList.Entry.EntryType.Party));
+                partyList.Entries.Add(PartyMemberToEntry(addonPartyList.PartyMember, i, SortedPartyList.Entry.EntryType.Party));
             }
 
-            for (var i = 0; i < partyList.TrustCount; ++i)
+            for (int i = 0; i < partyList.TrustCount; ++i)
             {
-                partyList.Entries.Add(TrustMemberToEntry(addonPartyList.TrustMembers, i,
-                                                         SortedPartyList.Entry.EntryType.Trust));
+                partyList.Entries.Add(PartyMemberToEntry(addonPartyList.PartyMember, i, SortedPartyList.Entry.EntryType.Party));
             }
 
             return JObject.FromObject(partyList);
         }
 
-        private static unsafe SortedPartyList.Entry PartyMemberToEntry(
-            Span<AddonPartyList.PartyListMemberStruct> partyMembers, int index, SortedPartyList.Entry.EntryType type)
+        private SortedPartyList.Entry PartyMemberToEntry(dynamic partyMemberListStruct, int index, SortedPartyList.Entry.EntryType type)
         {
-            var member = partyMembers[index];
-            return TextNodeToEntry(member.Name, index, type);
-        }
-        
-        private static unsafe SortedPartyList.Entry TrustMemberToEntry(
-            Span<AddonPartyList.PartyListMemberStruct> trustMembers, int index, SortedPartyList.Entry.EntryType type)
-        {
-            var member = trustMembers[index];
-            return TextNodeToEntry(member.Name, index, type);
-        }
-        
-        private static unsafe SortedPartyList.Entry TextNodeToEntry(
-            AtkTextNode* textNode, int index, SortedPartyList.Entry.EntryType type)
-        {
-            var nameStr = textNode == null ? "" : textNode->NodeText.ToString();
+            string key = type.ToString() + "Member" + index;
+            var member = partyMemberListStruct[key];
 
+            var name = member.Name;
+            var nameStr = name.NodeText;
+            if (nameStr == null)
+            {
+                nameStr = "";
+            }
             // Trim the utf8 chars at the start of the string by splitting on first space
             // Example raw string:
             // " Player Name"
@@ -108,7 +115,7 @@ namespace RainbowMage.OverlayPlugin.EventSources
             // "E06A" "E069" "E060" "20" etc
             // This translates to the following text in the special FFXIV UTF font
             // "Lv" "9" "0" " " etc
-            var parts = nameStr.Split(new[] { ' ' }, 2);
+            var parts = nameStr.Split(new char[] { ' ' }, 2);
             if (parts.Length > 1)
             {
                 nameStr = parts[1];
@@ -129,37 +136,42 @@ namespace RainbowMage.OverlayPlugin.EventSources
                 return null;
             }
 
-            var addon = atkStageMemory.GetAddon(key);
+            dynamic addon = atkStageMemory.GetAddon(key);
 
             if (addon == null)
             {
                 return null;
             }
 
-            static void HandleDeserializationError(object sender, ErrorEventArgs errorArgs) => 
-                errorArgs.ErrorContext.Handled = true;
-
-            var settings = new Newtonsoft.Json.JsonSerializerSettings
-            {
-                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore,
-                Error = HandleDeserializationError
-            };
+            var settings = new Newtonsoft.Json.JsonSerializerSettings();
+            settings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
             var serializer = Newtonsoft.Json.JsonSerializer.CreateDefault(settings);
 
-            var jObject = JObject.FromObject(addon, serializer);
+            var jobj = JObject.FromObject(addon, serializer);
 
-            return jObject;
+            return jobj;
         }
 
-        public override void Start() { }
+        public override Control CreateConfigControl()
+        {
+            return null;
+        }
+
+        public override void Start()
+        {
+        }
 
         public override void LoadConfig(IPluginConfig cfg)
         {
             Config = container.Resolve<BuiltinEventConfig>();
         }
 
-        public override void SaveConfig(IPluginConfig config) { }
+        public override void SaveConfig(IPluginConfig config)
+        {
+        }
 
-        protected override void Update() { }
+        protected override void Update()
+        {
+        }
     }
 }
--- OverlayPlugin.Core/MemoryProcessors/JobGauge/JobGaugeMemoryManager.cs
+++ OverlayPlugin.Core/MemoryProcessors/JobGauge/JobGaugeMemoryManager.cs
@@ -1,10 +1,11 @@
+﻿
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
-using FFXIVClientStructs.FFXIV.Client.Game.Gauge;
+using FFXIVClientStructs.Global.FFXIV.Client.Game.Gauge;
 #if !DEBUG
 using Newtonsoft.Json;
 #endif
